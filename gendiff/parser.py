b224a5378d7ecddcdb2d870cdbde4c6fa8fd1bba
akasmall <aka_sm@ro.ru>
2024-02-05 00:49:54 +0300

    сформировал словарь диффов

--- gendiff/parser.py
+++ gendiff/parser.py
@@ -1,50 +1,60 @@
 from gendiff.string_diff import build_str_diff
 
+ADDED = "added"
+REMOVED = "removed"
+CHANGED = "changed"
+UNCHANGED = "unchanged"
+NESTED = "nested"
+
 
 def format_bool_dict(data_dict):
     if isinstance(data_dict, bool):
         return str(data_dict).lower()
+    if data_dict is None:
+        return "null"
     return data_dict
 
 
-def add_union_keys(dict1, dict2, keys_union):
-    new_dict = dict()
-    for key_ in keys_union:
+def go_all_keys(dict1, dict2, all_keys):
+    dict_diff = {}
+    for key_ in all_keys:
         if key_ in dict1 and key_ not in dict2:
-            new_dict[key_] = ("removed", format_bool_dict(dict1[key_]))
+            dict_diff[key_] = (REMOVED, format_bool_dict(dict1[key_]))
         if key_ not in dict1 and key_ in dict2:
-            new_dict[key_] = ("added", format_bool_dict(dict2[key_]))
-    return new_dict
+            dict_diff[key_] = (ADDED, format_bool_dict(dict2[key_]))
+        if key_ in dict1 and key_ in dict2 and dict1[key_] == dict2[key_]:
+            dict_diff[key_] = (UNCHANGED, format_bool_dict(dict1[key_]))
+        if key_ in dict1 and key_ in dict2 and dict1[key_] != dict2[key_]:
+            dict_diff[key_] = (CHANGED,
+                               format_bool_dict(dict1[key_]),
+                               format_bool_dict(dict2[key_])
+                               )
+    return dict_diff
 
 
-def add_compound_keys(dict1, dict2, keys_compound):
-    new_dict = dict()
-    for key_ in keys_compound:
-        if dict1[key_] != dict2[key_]:
-            new_dict[key_] = (
-                "changed",
-                format_bool_dict(dict1[key_]),
-                format_bool_dict(dict2[key_])
-            )
-        else:
-            new_dict[key_] = ("unchanged", format_bool_dict(dict1[key_]))
-    return new_dict
-
-
+# def compare_dict(dict1, dict2, res_diff={}):
 def compare_dict(dict1, dict2):
-    dict_diff = dict()
-    keys_union = set(dict1.keys()) ^ set(dict2.keys())
-    keys_compound = set(dict1.keys()) & set(dict2.keys())
-    dict_diff.update(add_union_keys(dict1, dict2, keys_union))
-    dict_diff.update(add_compound_keys(dict1, dict2, keys_compound))
-    # print({k: dict_diff[k] for k in sorted(dict_diff)})
-    return {k: dict_diff[k] for k in sorted(dict_diff)}
+    dict_diff = {}
+    all_keys = dict1.keys() | dict2.keys()
+    dict_diff = go_all_keys(dict1, dict2, all_keys)
+
+    dict_diff = {k: dict_diff[k] for k in sorted(dict_diff)}
+    res_diff = {}
+    for i in dict_diff:
+        if (dict_diff[i][0] == CHANGED
+                and isinstance(dict_diff[i][1], dict)
+                and isinstance(dict_diff[i][2], dict)):
+            res_diff[i] = (NESTED, dict(compare_dict(
+                dict_diff[i][1],
+                dict_diff[i][2]
+            )))
+        else:
+            res_diff[i] = dict_diff[i]
+    return res_diff
 
 
 def parser_data(data1, data2):
-    format_data1 = format_bool_dict(data1)
-    format_data2 = format_bool_dict(data2)
-    dict_diff = compare_dict(format_data1, format_data2)
+    dict_diff = compare_dict(data1, data2)
     print(dict_diff)
     str_dict_diff = build_str_diff(dict_diff)
     return str_dict_diff
@@ -53,3 +63,46 @@ def parser_data(data1, data2):
 # removed(удаленный),
 # changed(измененный),
 # unchanged(неизменный)
+# nested(вложенный)
+
+    # keys_union = set(dict1.keys()) ^ set(dict2.keys())
+    # keys_compound = set(dict1.keys()) & set(dict2.keys())
+    # dict_diff.update(add_union_keys(dict1, dict2, keys_union))
+    # dict_diff.update(add_compound_keys(dict1, dict2, keys_compound))
+
+    # for key_ in dict1:
+    #     if key_ in dict2 and dict1[key_] != dict2[key_]:
+    #         dict_diff[key_] = (
+    #             CHANGED,
+    #             format_bool_dict(dict1[key_]),
+    #             format_bool_dict(dict2[key_])
+    #         )
+    #     elif key_ in dict1 and key_ not in dict2:
+    #         dict_diff[key_] = (REMOVED, format_bool_dict(dict1[key_]))
+    #     elif key_ in dict2 and dict1[key_] == dict2[key_]:
+    #         dict_diff[key_] = (UNCHANGED, format_bool_dict(dict1[key_]))
+    # for key_ in dict2:
+    #     if key_ in dict2 and key_ not in dict1:
+    #         dict_diff[key_] = (ADDED, format_bool_dict(dict2[key_]))
+# def add_union_keys(dict1, dict2, keys_union):
+#     new_dict = dict()
+#     for key_ in keys_union:
+#         if key_ in dict1 and key_ not in dict2:
+#             new_dict[key_] = (REMOVED, format_bool_dict(dict1[key_]))
+#         if key_ not in dict1 and key_ in dict2:
+#             new_dict[key_] = (ADDED, format_bool_dict(dict2[key_]))
+#     return new_dict
+
+
+# def add_compound_keys(dict1, dict2, keys_compound):
+#     new_dict = dict()
+#     for key_ in keys_compound:
+#         if dict1[key_] != dict2[key_]:
+#             new_dict[key_] = (
+#                 CHANGED,
+#                 format_bool_dict(dict1[key_]),
+#                 format_bool_dict(dict2[key_])
+#             )
+#         else:
+#             new_dict[key_] = (UNCHANGED, format_bool_dict(dict1[key_]))
+#     return new_dict
--- gendiff/string_diff.py
+++ gendiff/string_diff.py
@@ -1,15 +1,37 @@
+MINUS = " - "
+PLUS = " + "
+SPACE = "   "
+INDENT = "  "
+
+
+def collecting_str(text):
+    txt_str = ""
+    for i in text:
+        text[i] = 1
+    return txt_str
+
+
 def build_str_diff(dict_diff):
-    str_diff = "{\n"
-    for key_ in dict_diff:
-        if dict_diff[key_][0] == 'removed':
-            str_diff += f"  - {key_}: {dict_diff[key_][1]}\n"
-        elif dict_diff[key_][0] == 'unchanged':
-            str_diff += f"    {key_}: {dict_diff[key_][1]}\n"
-        elif dict_diff[key_][0] == 'added':
-            str_diff += f"  + {key_}: {dict_diff[key_][1]}\n"
-        elif dict_diff[key_][0] == 'changed':
-            str_diff += f"  - {key_}: {dict_diff[key_][1]}\n"
-            str_diff += f"  + {key_}: {dict_diff[key_][2]}\n"
+    str_diff = ""
+
+    # str_diff = collecting_str(dict_diff)
+    str_diff = "{\n" + str_diff
     str_diff += "}"
-    print(str_diff)
-    return str_diff
+    return dict_diff
+
+
+# def build_str_diff(dict_diff):
+#     str_diff = "{\n"
+#     for key_ in dict_diff:
+#         if dict_diff[key_][0] == 'removed':
+#             str_diff += f"  - {key_}: {dict_diff[key_][1]}\n"
+#         elif dict_diff[key_][0] == 'unchanged':
+#             str_diff += f"    {key_}: {dict_diff[key_][1]}\n"
+#         elif dict_diff[key_][0] == 'added':
+#             str_diff += f"  + {key_}: {dict_diff[key_][1]}\n"
+#         elif dict_diff[key_][0] == 'changed':
+#             str_diff += f"  - {key_}: {dict_diff[key_][1]}\n"
+#             str_diff += f"  + {key_}: {dict_diff[key_][2]}\n"
+#     str_diff += "}"
+#     print(str_diff)
+#     return str_diff
