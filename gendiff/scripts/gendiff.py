47245318414711f3de035344ff009cf583fc7036
akasmall <aka_sm@ro.ru>
2024-03-31 19:10:11 +0300

    проект 2 - исправление ошибок теста hexlet-check.yml вер. 03

--- gendiff/scripts/gendiff.py
+++ gendiff/scripts/gendiff.py
@@ -1,14 +1,13 @@
 #!/usr/bin/env python3.10
 from gendiff.cli import get_arguments_parsed
-# from gendiff.generate_diff import generate_diff
-from gendiff import generate_diff
+from gendiff.generate_diff import generate_diff
 
 
 def main():
 
     args_ = get_arguments_parsed()
     print(
-        generate_diff.generate_diff(
+        generate_diff(
             args_.first_file,
             args_.second_file,
             formatter=args_.format
