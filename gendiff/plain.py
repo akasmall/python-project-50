a34d4ae4fcd04ba00b23666aeb2586dab9b6b0ad
akasmall <aka_sm@ro.ru>
2024-04-07 20:21:17 +0300

    проект 2 - исправление ошибок теста hexlet-check.yml вер. 09

--- gendiff/plain.py
+++ gendiff/plain.py
@@ -10,6 +10,8 @@ UNCHANGED = "unchanged"
 def get_bool_null_str(value_):
     if isinstance(value_, bool) or value_ is None:
         result = json.dumps(value_)
+    elif isinstance(value_, (int, float)):
+        result = f"\'{str(value_)}\'"
     else:
         result = f"\'{value_}\'"
     return result
